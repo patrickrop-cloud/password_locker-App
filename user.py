4dfb971d8b26c8fb7c2078eacbc7058d03a599ea
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-03 00:56:00 +0300

    create files for classes  user accounts,credentials and test.

---
+++ user.py
@@ -0,0 +1,20 @@
+#user.py
+class User:
+    '''
+    This is a class that generates new user instance.
+    '''
+
+    def __init__(self, username, password):
+        '''
+        properties of instance in user class.
+        '''
+
+        self.username = username
+        self.password = password
+
+    user_List = []
+
+
+        
+
+
