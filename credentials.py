3a8ebe898dd989229407145381d780701d9c0f6e
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-05 02:51:20 +0300

    created credentials class to save username and password.

---
+++ credentials.py
@@ -0,0 +1,15 @@
+from user import User #importing user class.
+
+class Credentials:
+    '''
+    This is a class to create credentials instance.
+    '''
+    useer_credentials = []
+
+
+
+
+
+
+
+
