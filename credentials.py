5b7c0e6d23d48521365da74217aa37f11ccf8e4c
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-09-05 03:04:38 +0300

    login credentials.

--- credentials.py
+++ credentials.py
@@ -1,14 +1,21 @@
 from user import User #importing user class.
 
-class Credentials:
+class Logins:
     '''
-    This is a class to create credentials instance.
+    This is a class to create login credentials instance.
     '''
-    useer_credentials = []
+    user_logins = []
 
 def __init__(self, save_username, save_password):
     self.save_username = save_username
     self.save_password = save_password
+
+def save_logins(self):
+    '''
+    This saves users' login credentials
+    '''
+
+    User.user_logins.append(self) #saves new user credentials and append.
 
 
 
